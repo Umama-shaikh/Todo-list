86cf28f27e6b76deb25bd524faa9c64ca3b3639d
Umama-shaikh <160320031+Umama-shaikh@users.noreply.github.com>
2024-04-28 21:24:25 +0500

    added new features , such as : editing and deleting

--- main.ts
+++ main.ts
@@ -27,3 +27,55 @@ while(condition)
         condition = addTask.addMore
         console.log(todos)
     }
+
+let editOrDelete = await inquirer.prompt(
+    [
+        {
+            name : "action",
+            type : "list",
+            choices : ["editTask", "deleteTask", "exit"],
+            message : "what would you like to do."
+        }
+    ]
+); 
+
+if (editOrDelete.action === "editTask") {
+    let index = await inquirer.prompt(
+        [
+            {
+                name : "index",
+                type : "number",
+                message : "Enter the index of the task you want to edit: "
+            }
+        ]
+    );
+    let updatedTask = await inquirer.prompt(
+        [
+            {
+                name : "todo",
+                type : "input",
+                message: "Enter the updated task content. "
+            }
+        ]
+    );
+
+    todos[index.index] = updatedTask.todo;
+    console.log("Task updated successfully!");
+} else if (editOrDelete.action === "deleteTask") {
+    let index = await inquirer.prompt(
+        [
+            {
+                name : "index",
+                type : "number",
+                message : "Enter the index of the task you want to delete: "
+            }
+        ]
+    );
+
+    todos.splice(index.index, 1);
+    console.log("Task deleted successfully!");
+} else {
+    console.log("Exiting...");
+}
+
+console.log("final to-dos:" , todos );
